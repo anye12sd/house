4e2ccf2a58e2655fff0b4381b7f97073181e9f87
anye1994 <541890086@qq.com>
2016-11-28 17:07:49 +0800

    确认提交

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
-let express = require('express');
-let path = require('path');
+const express = require('express');
+const path = require('path');
+const proxy = require('http-proxy-middleware');
+const config = require('./config');
 
-let app = express();
+const app = global.app = express();
 
 //开发环境
+app.use('/', express.static(path.join(__dirname, config.path)));
 app.use('/dev/views', express.static(__dirname + '/views'));
 app.use('/dev/public', express.static(__dirname + '/public'));
 
 //部署环境
+app.use('/', express.static(path.join(__dirname, config.path)));
 app.use('/', express.static(__dirname + '/dist'));
 app.use('/public', express.static(__dirname + '/dist/public'));
+
+app.use('/api', proxy({
+  target: 'http://123.207.151.199:3000', // target host
+  changeOrigin: true,            // needed for virtual hosted sites
+  ws: true,                      // proxy websockets
+  cookieRewrite: true,
+  pathRewrite: {
+    '^/api/': '/'
+  }
+}));
 
 app.listen(16086, () => {
   console.log('server start at 16086')
---
+++ config/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  env: "dev",
+  enableCluster: false,
+  port: 7001,
+  assetsPath: "./public",
+  viewCache: false,
+  viewPath: 'views',
+  debug: true,
+  path:'views'
+};
